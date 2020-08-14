48a602b5439446985430ab3aa5850e2a0977c2be
Ishar Maharjan <isharmaharjan123@gmail.com>
2020-08-14 21:09:31 +0545

    Cheat system added (for now)

--- scratchy.py
+++ scratchy.py
@@ -10,7 +10,8 @@ colors = {
     'black' : (0,0,0),
     'red'  : (255,0,0),
     'blue' : (0,0,255),
-    'green': (0,255,0)
+    'green': (0,255,0),
+    'yellow': (0,255,255)
 }
 
 clock = pygame.time.Clock()
@@ -21,23 +22,43 @@ pygame.display.set_caption('Scratchy')
 screen.fill(colors.get('white'))
 
 draw = True
+cheats = {
+    'cr': 'red',
+    'cb': 'blue',
+    'cg': 'green',
+    'cy': 'yellow',
+    'ckb': 'black',
+    'cw': 'white'
+}
+cheat = ''
+color = 'red'
 
 while True:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
         if event.type == pygame.KEYDOWN:
-            key = pygame.key.get_pressed()
-            if key[pygame.K_SPACE]:
+            key = event.key
+            if key == pygame.K_SPACE:
                 draw = not draw
+            elif key == pygame.K_ESCAPE:
+                cheat = ''
+                print('Cheat cleared.')
+            else:
+                cheat += chr(key)
+            print(f'Cheat: "{cheat}"')
+
+    if cheat in cheats.keys():
+        color = cheats[cheat]
+        print(f'Changed the color to {cheats[cheat]}.')
+        cheat = ''
 
 
     if pygame.mouse.get_pressed() == (1,0,0):
         pos = pygame.mouse.get_pos()
         if draw:
-            pygame.draw.rect(screen, colors.get('red'), (pos[0], pos[1], 5, 5))
+            pygame.draw.rect(screen, colors.get(color), (pos[0], pos[1], 7, 7))
         else:
             pygame.draw.circle(screen, colors.get('white'), pos, 30)
 
